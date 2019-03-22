4c102da670fcd4300f0ac5d72c8e3d5f53c9be4d
Negbert <negbert91@gmail.com>
2019-03-22 12:34:21 -0400

    arrays

--- Grades.Tests/Types/ReferenceTypeTests.cs
+++ Grades.Tests/Types/ReferenceTypeTests.cs
@@ -12,6 +12,22 @@ namespace Grades.Tests.Types
     {
 
         [TestMethod]
+        public void UsingArrays()
+        {
+            float[] grades;
+            grades = new float[3];
+
+            AddGrades(grades);
+
+            Assert.AreEqual(89.1f, grades[1]);
+        }
+
+        private void AddGrades(float[] grades)
+        {
+            grades[1] = 89.1f;
+        }
+
+        [TestMethod]
         public void UppercaseString()
         {
             string name = "nate";
@@ -26,7 +42,7 @@ namespace Grades.Tests.Types
             DateTime date = new DateTime(2015, 1, 1);
              date = date.AddDays(1);
 
-            Assert.AreEqual(2, date.Day)
+            Assert.AreEqual(2, date.Day);
         }
 
         private void GiveBookAname(GradeBook book)
